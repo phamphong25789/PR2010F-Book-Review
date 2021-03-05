b864a19f3d77497db1b6e63eb78f19fe41eb248a
phamphong25789 <phamphong25789@gmail.com>
2021-03-05 14:17:18 +0700

    suaprofilev02

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,63 +1,63 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-	before_action :find_user, only: [:show, :edit, :update, :destroy]
-	before_action :correct_user, only: [:edit, :update]
-	
-	def index
-		@users = User.paginate(page: params[:page])
-	end
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
 
-	def new
-		@user = User.new
-	end
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
-	def create
-		@user = User.new user_params
-		if @user.save
-			log_in @user
-			flash[:success] = "Welcome to the Sample App!"
-			redirect_to @user
-		else
-			render :new
-		end
-	end
+  def new
+    @user = User.new
+  end
 
-	def show
-	end
+  def create
+    @user = User.new user_params
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
 
-	def edit
-	end
+  def show
+  end
 
-	def update
-		if @user.update user_params
-			flash[:success] = "Profile updated"
-			redirect_to @user
-		else
-			render :edit
-		end
-	end
+  def edit
+  end
 
-	private
-		def user_params
-			params.require(:user).permit :name, :email, :password,:password_confirmation
-		end
+  def update
+    if @user.update user_params
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
 
-		def find_user
-			@user =  User.find_by id: params[:id]
-			unless @user.present?
-				flash[:success] = "User doesn't exist"
-				redirect_to users_url
-			end
-		end
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password,:password_confirmation
+  end
 
-		# Confirms the correct user.
-		def correct_user
-			redirect_to root_url unless @user.current_user? current_user
-		end
+  def find_user
+    @user =  User.find_by id: params[:id]
+    unless @user.present?
+      flash[:success] = "User doesn't exist"
+      redirect_to users_url
+    end
+  end
 
-		#Confirms an admin user
-		def admin_user
-			redirect_to root_url unless current_user.role_admin?
-		end
+  # Confirms the correct user.
+  def correct_user
+    redirect_to root_url unless @user.current_user? current_user
+  end
+
+  #Confirms an admin user
+  def admin_user
+    redirect_to root_url unless current_user.role_admin?
+  end
 
 end
